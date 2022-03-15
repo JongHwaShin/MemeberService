b52dab7c74abb19cf16e9698abda579bca192964
JongHwa Shin <tls1403@naver.com>
2022-03-15 23:11:05 +0900

    Add about Bean

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,24 +9,34 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;  
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService(){
 
         return new MemberServiceImpl(memberRepository());
     }
+    @Bean
+    public MemberRepository memberRepository() {
 
-    private MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-
+    @Bean
     public OrderService orderService(){
-        return new OrderServiceImpl(memberRepository(),discountPolicy());
+
+        return new OrderServiceImpl(
+                memberRepository(),
+                discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
-        return new RateDiscountPolicy();
+
+          return new RateDiscountPolicy();
     }
 
 }
