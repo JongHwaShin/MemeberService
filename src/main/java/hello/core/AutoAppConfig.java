bc63cdcfdb88961bf281b60f9fc9e51b986c305c
JongHwa Shin <tls1403@naver.com>
2022-03-20 23:24:11 +0900

    End study

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,19 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        basePackages = "hello.core",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Configuration.class) //Appconfig도 자동등록되는걸 막기위해
+)
+public class AutoAppConfig {
+
+
+
+}
